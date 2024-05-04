80c58be983ff530a5590c3bee539fd7d18aed582
Yun, Seongeun <0420yun@gamil.com>
2024-05-04 23:31:01 +0900

    [Feat] #11 - Add Sidebar Hide

--- FootMark/FootMark/Presentation/Sidebar/ViewController/MainContentViewController.swift
+++ FootMark/FootMark/Presentation/Sidebar/ViewController/MainContentViewController.swift
@@ -20,7 +20,9 @@ class MainContentViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor.White()
+        
+        setupDimmingView()
     }
     
     override func setAddTarget() {
@@ -49,14 +51,56 @@ class MainContentViewController: BaseViewController {
         let sidebarWidth = self.view.frame.width * 2 / 3
         sidebarVC.view.frame = CGRect(x: -sidebarWidth, y: 0, width: sidebarWidth, height: self.view.frame.height)
         
-        UIView.animate(withDuration: 0.5) {
+        UIView.animate(withDuration: 0.5, animations: {
             sidebarVC.view.frame = CGRect(x: 0, y: 0, width: sidebarWidth, height: self.view.frame.height)
             self.dimmingView.alpha = 1.0
+        }) { (finished) in
+            if finished {
+                self.isSidebarPresented = true
+            }
         }
-        self.isSidebarPresented = true
     }
 
+    func setupDimmingView() {
+        dimmingView.backgroundColor = UIColor.black.withAlphaComponent(0.0)
+        dimmingView.alpha = 0.0
+        view.addSubview(dimmingView)
+        
+        dimmingView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideSidebar))
+        dimmingView.addGestureRecognizer(tapGesture)
+    }
+
+    
+    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+        hideSidebar()
+    }
+    
     @objc func sidebarButtonTapped() {
-        showSidebar()
+        if isSidebarPresented {
+            hideSidebar()
+        } else {
+            showSidebar()
+        }
+    }
+
+    @objc func hideSidebar() {
+        guard let sidebarVC = sidebarVC else { return }
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            sidebarVC.view.frame = CGRect(x: -self.view.frame.width, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            self.dimmingView.alpha = 0.0
+        }) { (finished) in
+            sidebarVC.willMove(toParent: nil)
+            sidebarVC.view.removeFromSuperview()
+            sidebarVC.removeFromParent()
+            self.isSidebarPresented = false
+        }
     }
 }
